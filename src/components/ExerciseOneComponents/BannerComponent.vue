7c85f82a466680266d14627e6ccead4046705409
denden07 <ilovenjr21@gmail.com>
2024-07-21 15:49:33 +0800

    additional changes on desktop banner

--- src/components/ExerciseOneComponents/BannerComponent.vue
+++ src/components/ExerciseOneComponents/BannerComponent.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="banner-container flex-vertical-center">
         <div class="banner-header">
-            <img src="https://via.placeholder.com/1920x650" alt="">
+            <img class="show-desktop-only" src="https://via.placeholder.com/1920x650" alt="">
+            <img class="show-mobile-only" src="https://via.placeholder.com/600x600" alt="">
             <div class="banner-header-text">
                 <h1>Hello Developer</h1>
                 <div class="spacer"></div>
